8a7b2c09dfa71ab1cdb53e7a26f530272d02300d
Adithya Abraham Philip <adithyaphilip@github.com>
2015-04-19 15:11:10 +0530

    allows updating contacts

--- application/models/contact.php
+++ application/models/contact.php
@@ -11,9 +11,17 @@ class Contact extends ExposedModel {
         $batch = [];
         foreach ($numbers as $value) {
             $number = substr($value, -10);
-            $batch[] = ['userid'=>$params['userid'],'number'=>$number];
+            $exist_contact = $this->getEntries(['userid'=>1,'number'=>$number], ['userid','number']);
+            if(count($exist_contact)==0) {
+                $batch[] = ['userid'=>$params['userid'],'number'=>$number];
+            }
         }
-        return $this->db->insert_batch($this->getTable(),$batch);
+        if(count($batch)!=0) {
+            return $this->db->insert_batch($this->getTable(),$batch);
+        }
+        else {
+            throw new Exception("No contacts to update");
+        }
     }
 }
 
